cb51838748f473d0f7b75913e4e54af65937445f
CarranzaIvan <113496779+CarranzaIvan@users.noreply.github.com>
2023-06-27 16:49:40 -0600

    Prueba 1 de Commit

---
+++ src/java/cs21024sintaxis/JavaCS21024Sintaxis.java
@@ -0,0 +1,11 @@
+
+package java.cs21024sintaxis;
+
+// @author Iván Alexander Carranza Sánchez
+
+public class JavaCS21024Sintaxis {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
