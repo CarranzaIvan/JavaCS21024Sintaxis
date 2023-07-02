eceac40b2ab40e340d12139f72038982450f8f67
CarranzaIvan <113496779+CarranzaIvan@users.noreply.github.com>
2023-07-02 11:51:29 -0600

    Declaración y inicialización de variables (Tipos de datos)

--- src/java/cs21024sintaxis/JavaCS21024Sintaxis.java
+++ src/java/cs21024sintaxis/JavaCS21024Sintaxis.java
@@ -5,7 +5,33 @@ package java.cs21024sintaxis;
 
 public class JavaCS21024Sintaxis {
     public static void main(String[] args) {
-        //Mejora
+        /*
+             ||===============================================||
+             ||    DECLARACIÓN Y INICIALIZACIÓN DE VARIABLES      ||
+             ||          (TIPOS DE VARIABLES Y CONVERSIÓN)                 ||
+             ||===============================================||
+        */
+        
+         //---------DECLARACIÓN DE VARIABLES Y INICIALIZACIÓN---------
+         //Númericas
+            //Enteros
+            byte bit = 0; // 1 Byte
+            short numeroCorto = 9999; // 2 Byte
+            int entero = 9999; // 4 Byte
+            long largo = 949494L; // 8 Byte
+            
+            //Decimales
+            float flotante = 4.4f; // 4 Byte 
+            double real = 4.4; // 8 Byte
+            
+         //Alfanumericos
+            char caracter = 'C'; // 2 Byte (Codigo Unicode)
+            
+         // Logico
+            boolean logico = false; // 1 Byte (true or false)
+            
+        //---------CONVERSIÓN DE UN VALOR A OTRO---------
+        
     }
     
 }
