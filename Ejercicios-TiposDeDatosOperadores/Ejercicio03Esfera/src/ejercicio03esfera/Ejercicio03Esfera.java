a27354812f47d9dfdc21aa1e91c100de1ad9e9fe
CarranzaIvan <113496779+CarranzaIvan@users.noreply.github.com>
2023-07-06 18:16:32 -0600

    Actualización de problemas

---
+++ Ejercicios-TiposDeDatosOperadores/Ejercicio03Esfera/src/ejercicio03esfera/Ejercicio03Esfera.java
@@ -0,0 +1,16 @@
+package ejercicio03esfera;
+
+// @author Iván Alexander Carranza Sánchez
+/* Construir un programa que, dado el radio de una esfera, calcule y devuelva por pantalla el valor de la superficie y el volumen de la esfera correspondiente. Para obtener el valor de PI, utilizar la variable estática Math.PI.
+
+Volumen esfera = (4/3)PIR3
+
+Superficie esfera = 4PIR2
+*/
+
+public class Ejercicio03Esfera {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
